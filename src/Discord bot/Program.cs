9a3fbc92c3ec2baa2cbf30ec14ecd9a26cd7267e
Amooti <cedrikpedersen@gmail.com>
2019-09-20 15:23:13 +0200

    Der Übermensch can turn on
    Medic

--- src/Discord bot/Program.cs
+++ src/Discord bot/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace Discord_bot
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+        
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            var token = "NjI0NTkxOTk5OTc0MjQ0MzUz.XYTPWw.j5jMDR5CN0Tn2quUkm5uR3vHGBM";
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
