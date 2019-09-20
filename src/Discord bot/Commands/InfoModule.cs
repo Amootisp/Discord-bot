8882b81164cbb520f51b5ea9460deb5f31944544
Amooti <cedrikpedersen@gmail.com>
2019-09-20 16:00:24 +0200

    Forgot to stage

--- src/Discord bot/Commands/InfoModule.cs
+++ src/Discord bot/Commands/InfoModule.cs
@@ -1,7 +1,13 @@
+using System.Threading.Tasks;
+using Discord.Commands;
+
 namespace Discord_bot.Commands
 {
-    public class InfoModule
+    public class InfoModule : ModuleBase<SocketCommandContext>
     {
-        
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+            => ReplyAsync(echo);
     }
 }
